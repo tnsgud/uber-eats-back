b858c1b1eaabafe4fec1a1ff3ed82c2970742ee4
tnsgud <60930113+tnsgud@users.noreply.github.com>
2025-04-22 23:40:02 +0900

    ✨Database Configuration Section
    * ➕ Add TypeORM, sqlite, cross-env
    
    * 🙈 Update gitignore
    
    * 🚩 Add ConfigModule, TypeOrmModule
    
    * ➕ Add joi
    
    * 🚩 Add env validation using Joi

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common'
+import * as Joi from 'joi'
 import { GraphQLModule } from '@nestjs/graphql'
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
 import { RestaurantsModule } from './restaurants/restaurants.module'
-import { join } from 'path'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('dev', 'prod'),
+      }),
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true,
@@ -15,6 +25,12 @@ import { join } from 'path'
           'request.credentials': 'same-origin',
         },
       },
+    }),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      synchronize: true,
+      logging: true,
     }),
     RestaurantsModule,
   ],
