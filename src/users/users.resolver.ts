63a223ac4a7c84314a905f3b38c2b4b6af136eed
tnsgud <qkrtnsgud0229@gmail.com>
2025-04-27 08:32:56 +0000

    ♻️ Refactor Error handing

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -20,16 +20,12 @@ export class UsersResolver {
     @Args('input') createAccountInput: CreateAccountInput,
   ): Promise<CreateAccountOutput> {
     try {
-      const error = await this.usersService.createAccount(createAccountInput)
+      const { ok, error } =
+        await this.usersService.createAccount(createAccountInput)
 
-      if (error) {
-        return {
-          ok: false,
-          error,
-        }
-      }
       return {
-        ok: true,
+        ok,
+        error,
       }
     } catch (error) {
       return {
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -14,16 +14,18 @@ export class UsersService {
     email,
     password,
     role,
-  }: CreateAccountInput): Promise<string | undefined> {
+  }: CreateAccountInput): Promise<{ ok: boolean; error?: string }> {
     try {
       const exists = await this.users.findOne({ where: { email } })
 
-      if (exists) return 'There is a user with that email already'
+      if (exists)
+        return { ok: false, error: 'There is a user with that email already' }
 
       await this.users.save(this.users.create({ email, password, role }))
+      return { ok: true }
     } catch (error) {
       console.log(error)
-      return "Couldn't create account"
+      return { ok: false, error: `Couldn't create account ${error}` }
     }
   }
 }
